1253d5d1ecf426594541429785a8b634398edfd4
pnguyen215 <nguyenhuuphuocit97@gmail.com>
2023-11-17 09:32:48 +0700

    :sparkles: feat: added telegram notify for database conn as middleware #8

--- internal/core/core.go
+++ internal/core/core.go
@@ -2,13 +2,21 @@ package core
 
 import (
 	"fmt"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis"
 	syncconf "github.com/sivaosorg/gocell/internal/syncConf"
+	"github.com/sivaosorg/gocell/pkg/constant"
+	"github.com/sivaosorg/govm/blueprint"
+	"github.com/sivaosorg/govm/bot/telegram"
 	"github.com/sivaosorg/govm/dbx"
+	"github.com/sivaosorg/govm/logger"
 	"github.com/sivaosorg/govm/server"
+	"github.com/sivaosorg/govm/timex"
+	"github.com/sivaosorg/govm/utils"
 	"github.com/sivaosorg/mysqlconn/mysqlconn"
 	"github.com/sivaosorg/postgresconn/postgresconn"
 	"github.com/sivaosorg/redisconn/redisconn"
@@ -41,6 +49,7 @@ func (c *CoreCommand) Execute(args []string) error {
 		return err
 	}
 	c.conn()
+	c.notify()
 	c.handler()
 	c.run()
 	select {} // Keep application running
@@ -54,8 +63,11 @@ func (c *CoreCommand) conn() {
 	syncconf.Conf.MySql.SetTimeout(10 * time.Second)
 	syncconf.Conf.Redis.SetTimeout(10 * time.Second)
 
-	// Instances
+	// Instances async
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		psql, s := postgresconn.NewClient(syncconf.Conf.Postgres)
 		if s.IsConnected {
 			defer psql.Close()
@@ -63,7 +75,9 @@ func (c *CoreCommand) conn() {
 		c.psql = psql
 		c.psqlStatus = s
 	}()
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		msql, s := mysqlconn.NewClient(syncconf.Conf.MySql)
 		if s.IsConnected {
 			defer msql.Close()
@@ -71,7 +85,9 @@ func (c *CoreCommand) conn() {
 		c.msql = msql
 		c.msqlStatus = s
 	}()
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		redis, s := redisconn.NewClient(syncconf.Conf.Redis)
 		if s.IsConnected {
 			defer redis.Close()
@@ -79,6 +95,7 @@ func (c *CoreCommand) conn() {
 		c.redis = redis
 		c.redisStatus = s
 	}()
+	wg.Wait()
 }
 
 func (c *CoreCommand) run() {
@@ -133,4 +150,39 @@ func (c *CoreCommand) snap(args []string) error {
 	syncconf.Conf = keys
 	syncconf.Params = params
 	return nil
+}
+
+func (c *CoreCommand) notify() {
+	conf, err := syncconf.Conf.FindTelegramSeeker(constant.TelegramKeyTenant1)
+	if err != nil {
+		logger.Errorf("Telegram Bot Notify", err)
+		return
+	}
+	if !conf.Config.IsEnabled {
+		return
+	}
+	go c.sendNotify("Psql Conn Alert", c.psqlStatus, conf)
+	go c.sendNotify("Mysql Conn Alert", c.msqlStatus, conf)
+	go c.sendNotify("Redis Conn Alert", c.redisStatus, conf)
+}
+
+func (c *CoreCommand) sendNotify(topic string, status dbx.Dbx, conf telegram.MultiTenantTelegramConfig) {
+	var builder strings.Builder
+	icon, _ := blueprint.TypeIcons[blueprint.TypeSuccess]
+	if !status.IsConnected {
+		icon, _ = blueprint.TypeIcons[blueprint.TypeError]
+	}
+	timestamp := timex.With(time.Now()).Format(timex.DateTimeFormYearMonthDayHourMinuteSecond)
+	builder.WriteString(fmt.Sprintf("%v %s\n", icon, topic))
+	builder.WriteString(fmt.Sprintf("tz: `%s`\n\n", timestamp))
+	builder.WriteString(fmt.Sprintf("connected: `%v`\n", status.IsConnected))
+	builder.WriteString(fmt.Sprintf("pid: `%v`\n", status.Pid))
+	if utils.IsNotEmpty(status.Message) {
+		builder.WriteString(fmt.Sprintf("message: `%v`\n", status.Message))
+	}
+	if status.Error != nil {
+		builder.WriteString(fmt.Sprintf("error: `%v`\n", status.Error.Error()))
+	}
+	svc := telegram.NewTelegramService(conf.Config, conf.Option)
+	svc.SendMessage(builder.String())
 }
